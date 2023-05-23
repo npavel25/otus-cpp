3f56e964d75d394177065ffb515d78018687db18
Pavel Nikolaev <npavel25@yandex.ru>
2023-05-23 11:57:05 +0300

    First version

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+//#include "build_number.h"
+
+int main()
+{
+	//std::cout << "build " << BUILD_NUMBER << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
