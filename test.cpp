a123121ff941205fb9d792e1e167cac0bf66497c
Pavel Nikolaev <npavel25@yandex.ru>
2023-05-23 12:57:35 +0300

    BUILD_NUMBER enabled

--- test.cpp
+++ test.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-//#include "build_number.h"
+#include "build_number.h"
 
 int main()
 {
-	//std::cout << "build " << BUILD_NUMBER << std::endl;
+	std::cout << "build " << BUILD_NUMBER << std::endl;
     std::cout << "Hello, World!" << std::endl;
 
     return 0;
