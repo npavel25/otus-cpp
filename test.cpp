aea944d71b759c64a2662db7c8fc2da91b44762b
Pavel Nikolaev <npavel25@yandex.ru>
2023-05-24 13:50:39 +0300

    just to run actions

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@
 int main()
 {
 	std::cout << "build " << BUILD_NUMBER << std::endl;
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;;
 
     return 0;
 }
